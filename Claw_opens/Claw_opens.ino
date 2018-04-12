887b86eba263374fde19db47f41ddc21afe377d1
Baian Elmazry <belmazry250@gmail.com>
2018-04-12 18:42:06 -0400

    Claw stuff

---
+++ Claw_opens/Claw_opens.ino
@@ -0,0 +1,51 @@
+// Include the Servo library 
+#include <Servo.h> 
+
+Servo myservo; 
+int x = 0;
+int servo_position=0;
+
+void setup() { 
+  Serial.print("Please Work!!!");
+  // We need to attach the servo to the used pin number 
+   myservo.attach(6); //attaches the servo on pin 6 to the servo object
+   Serial.begin(9600);
+   //x = 1;
+   // want x = 1
+
+}
+
+
+
+void loop(){
+  if(Serial.available())
+  {
+    x = Serial.read(); //read serial port    
+    x = x - '0';
+    
+    for(int i = 0)
+    
+   if(x == 1);
+  {
+      // Servo needs to open at 45 degrees:
+      for (servo_position=0; servo_position<=60; servo_position +=1)
+    {
+      myservo.write(servo_position);
+      delay(5); //or if styrofoam falling out, make delay longer
+      }
+  exit (0);
+  }
+  exit(0);
+ }
+ exit(0);
+}
+
+/* NOTES:
+  works with out the serial command thing 
+    -currently being weird when add serial read and it isnt even connected to the pi yet
+    -idk what is going on :/
+
+
+
+*/
+
---
+++ claw_with_rangefinder/claw_with_rangefinder.ino
@@ -0,0 +1,69 @@
+#include <Servo.h>                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
+
+// Variables:
+Servo myservo;
+int angle = 0;
+int val;
+#define trigPin A3
+#define echoPin 7
+
+
+// Initial setup:
+void setup(){
+  Serial.begin(9600); // Set the baudrate
+  myservo.attach(6); // Plug into pin 6
+  val = 0;
+    Serial.begin(9600);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  myservo.attach(6); //attaches the servo on pin 6 to the servo object
+ 
+}
+
+
+void loop()
+{
+    // put your main code here, to run repeatedly:
+long duration, distance;
+  digitalWrite(trigPin, LOW);  // Added this line
+  delayMicroseconds(2); // Added this line
+  digitalWrite(trigPin, HIGH);
+//  delayMicroseconds(1000); - Removed this line
+  delayMicroseconds(10); // Added this line
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distance = (duration/2) / 29.1;
+
+  Serial.print(distance);
+    Serial.println(" cm");
+ 
+  delay(500);
+
+ // If the digit is 1, then close the claw:
+    if(distance<10){
+      close_claw();
+    }
+    
+    // If the digit is 0, then open the claw:
+    if(distance>10){
+      open_claw();
+    }
+  }
+
+// Close claw function: 
+void close_claw(){
+  angle = 100; // 100 degrees seems to close claw in a secure way
+  myservo.write(angle); // send angle to the servo
+  delay(1); 
+}
+
+// Open claw function:
+void open_claw(){
+  angle = 15; // 15 degrees seems to open the claw wide enough for travelling purposes
+  myservo.write(angle); // send angle to the servo
+  delay(1); 
+}
+
+
+
+
